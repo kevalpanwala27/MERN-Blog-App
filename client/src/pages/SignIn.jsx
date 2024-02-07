0d96ad5c61a8d6e30236089d435f68e4494c76fa
Keval Panwala <107193741+kevalpanwala27@users.noreply.github.com>
2024-02-07 18:53:41 +0530

    add redux

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,6 +1,12 @@
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../../redux/user/userSlice";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
