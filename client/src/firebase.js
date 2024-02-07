edcc160d5a24200a2e3be0f85293bc23c00959be
Keval Panwala <107193741+kevalpanwala27@users.noreply.github.com>
2024-02-07 21:32:29 +0530

    Add OAuth functionality

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC0sgKECeGVMHSPezBYG1DFY_cnH3XOu-s",
+  authDomain: "mern-blog-app-f5bbb.firebaseapp.com",
+  projectId: "mern-blog-app-f5bbb",
+  storageBucket: "mern-blog-app-f5bbb.appspot.com",
+  messagingSenderId: "383286797817",
+  appId: "1:383286797817:web:578c5c91215d78bd538928",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,22 +1,21 @@
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import OAuth from "../components/OAuth";
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
   const [errorMessage, setErrorMessage] = useState(null);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
   };
-
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!formData.username || !formData.email || !formData.password) {
       return setErrorMessage("Please fill out all fields.");
-    } 
+    }
     try {
       setLoading(true);
       setErrorMessage(null);
@@ -25,21 +24,19 @@ export default function SignUp() {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
       });
-
       const data = await res.json();
       if (data.success === false) {
         return setErrorMessage(data.message);
       }
       setLoading(false);
       if (res.ok) {
-        navigate("/signin");
+        navigate("/sign-in");
       }
     } catch (error) {
       setErrorMessage(error.message);
       setLoading(false);
     }
   };
-
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
